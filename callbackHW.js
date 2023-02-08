0b4da6e444015f15eed6bef7c9e2eadcd032d18f
Nick Chernock <n.chernock@gmail.com>
2023-02-07 21:16:33 -0500

    Initial Commit

--- callbackHW.js
+++ callbackHW.js
@@ -3,18 +3,20 @@ function getButton() {
     let button = document.createElement('span');
     document.body.append(button);
     button.style.backgroundColor = 'blue';
-    button.innerHTML = "example";
     button.style.fontSize = '50px'
+    button.innerHTML = "Example";
+    button.className = 'btn';
     return button;
 }
 
 function redButton(button) {
     setTimeout(function() {
         button.style.backgroundColor = 'red';
+        button.style.color = 'white';
+        button.innerHTML = 'Changed'
     }, delay);
 };
 
 let delay = 2000;
 
 redButton(getButton(), delay);
-console.log('test');
